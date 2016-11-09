9264c68e528f5225f0c25386a322294908cb75c0
Satoshi Shiraishi <sachoco@gmail.com>
2016-11-09 22:34:37 +0100

    google-apps

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="paMekdgaIqWUPflcVYN9xHCB8G-NP03RVY3coT7OHOU" />
+
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
